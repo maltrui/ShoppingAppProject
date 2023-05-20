2e7e12b9f2be3242b12fc4e696dc1f53da4dc022
maltrui <mcaltrui@gmail.com>
2023-05-19 23:48:13 -0400

    NavBar

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,14 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import AuthPage from '../AuthPage/AuthPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
+import NavBar from '../../components/NavBar/NavBar';
 function App() {
   const [user, setUser] = useState({});
   return (
     <main className="App">
         { user ?
+        <>
+        <NavBar />
         <Routes>
           <Route path="/orders/new" element={<NewOrderPage />}/>
+          <Route path="/orders" element={<OrderHistoryPage />} />
         </Routes>
+        </>
         :
         <AuthPage />
         }
--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,6 +1,8 @@
 
 export default function OrderHistoryPage(){
+    return(
     <div>
         OrderHistoryPage
     </div>
+    )
 }
