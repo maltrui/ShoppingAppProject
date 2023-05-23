fdb3490ce5bebf30386baeee842370ac274b9608
maltrui <mcaltrui@gmail.com>
2023-05-23 00:28:17 -0400

    SignUp form

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,7 +5,7 @@ import AuthPage from '../AuthPage/AuthPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
 import NavBar from '../../components/NavBar/NavBar';
 function App() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   return (
     <main className="App">
         { user ?
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,11 @@
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
 
 export default function AuthPage(){
     return(
-    <div>
-        AuthPage
-    </div>
+    <main>
+        <h1>AuthPage</h1>
+        <SignUpForm />
+    </main>
     );
 }
 
