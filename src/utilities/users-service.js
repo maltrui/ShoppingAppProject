070fb0b416c5293ecc54673188ba2c631050c87f
maltrui <mcaltrui@gmail.com>
2023-06-28 20:15:54 -0400

    login and signup functions

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -25,3 +25,9 @@ export function getUser() {
 export function logOut() {
   localStorage.removeItem('token');
 }
+
+export async function login(credentials) {
+  const token = await usersAPI.login(credentials);
+  localStorage.setItem('token', token);
+  return getUser();
+}
